3214a7da1064c3004f94cc31456aecb31c6418f3
Mohnish <mohnish.a007@gmail.com>
2023-06-04 22:27:52 +0100

    Basic setup storybook react with tailwind css running

---
+++ storybook2/.storybook/preview.js
@@ -0,0 +1,16 @@
+//Import the tailwind css output file here
+import '../src/tailwindout.css';
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
